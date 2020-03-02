7b14cfef6e31c729c7ef4dbc23b6426472309257
MAGNO DE OLIVEIRA NEVES <magno.oneves@senacsp.edu.br>
2020-03-02 15:42:00 -0300

    Resolução de exercícios e conceitos novos

---
+++ lista/script.js
@@ -0,0 +1,21 @@
+let nomes = ['Wheslley', 'Aline', 'João'];
+let listElement = document.querySelector('ul');
+let inputElement = document.querySelector('input');
+
+function addItem(text) {
+    let liElement = document.createElement('li');
+    let textElement = document.createTextNode(text);
+    liElement.appendChild(textElement);
+    listElement.appendChild(liElement);
+}
+
+document.getElementById('btn').addEventListener('click', () => {
+    addItem(inputElement.value);
+    inputElement.value = '';
+});
+
+for(nome of nomes){
+    addItem(nome);
+}
+
+// nome pode ser qualquer palavra!!!
