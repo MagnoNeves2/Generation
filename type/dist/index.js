612d98bd413dcf5e8defd1e074a2237f7b18f7c0
Magno <magnoneves2@gmail.com>
2020-03-12 19:25:31 -0300

    Pratica de como começar um projeto em Node para Typescript e .gitignore

--- type/dist/index.js
+++ type/dist/index.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
+var port = 3000;
 app.get('/', function (req, res) {
-    res.send('Teste');
+    res.send('Estou testando essa bagaça, vai que neh! Hahahah');
 });
-app.listen(3000);
+app.listen(port);
