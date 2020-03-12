fda3dca15271fd7bb95969647a43632f827c22b3
Magno <magnoneves2@gmail.com>
2020-03-12 19:25:31 -0300

    Pratica de como começar um projeto em Node para Typescript e .gitignore

--- type/src/index.ts
+++ type/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-
 const app = express();
+let port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Testando');
+    res.send('FOI!!!')
 });
 
-app.listen(3000);
+app.listen(port);
+
+console.log(`Servidor rodando em http://localhost:${port}.`);
